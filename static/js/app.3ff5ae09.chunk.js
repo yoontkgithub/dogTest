7593a0769ed3f9a7a913e1c1ffe8c4cb7d462d14
taikeun yoon <yoontk@github.com>
2021-02-24 22:41:28 +0900

    Updates

---
+++ static/js/app.3ff5ae09.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{29:function(e,t,n){"use strict";n.d(t,"a",(function(){return i}));var a=n(52),r=n(0),o=n.n(r),c=n(3),s=n(46),u=n(11);function i(){return o.a.createElement(u.a,{style:p.container},o.a.createElement(s.a,null,"Open up App.js to start working on your app!"),o.a.createElement(a.a,{style:"auto"}))}var p=c.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}})},53:function(e,t,n){e.exports=n(66)}},[[53,1,2]]]);
+//# sourceMappingURL=app.3ff5ae09.chunk.js.map
